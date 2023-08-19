3238bf9947c757141c47fe529de84c649e4107c1
Geraldo Homero <geraldohomero@protonmail.com>
2023-08-19 17:32:59 -0300

    init commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello, world!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#151515',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+  }
 });
+
+
