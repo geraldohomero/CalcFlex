26001834f28632493915f2a44a2fb1a7f9f7a84b
Geraldo Homero <geraldohomero@protonmail.com>
2023-08-27 09:21:28 -0300

    update theme

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Appbar, TextInput, Button } from 'react-native-paper';
 
 const App = () => {
@@ -8,26 +8,43 @@ const App = () => {
   const [eta, setEta] = useState('');
 
   return (
-    <>
+    <View style={styles.container}>
       <Appbar.Header>
         <Appbar.Content title="Calculadora Flex" subtitle="Etanol ou Gasolina?"/>
       </Appbar.Header>
-      <TextInput
-        label="Preço da Gasolina"
-        value={gas}
-        onChangeText={text => setText(text)}
-      />
-        <TextInput
-        label="Preço do Etanol"
-        value={eta}
-        onChangeText={text => setEta(text)}
-      />
-      <Button mode="contained" onPress={() => console.log('Pressed')}>
-        Calcular
-      </Button>
-  </>
+        <View style={styles.body}>      
+          <TextInput
+            style={styles.input}
+            label="Preço da Gasolina"
+            value={gas}
+            onChangeText={text => setText(text)}
+          />
+            <TextInput
+            style={styles.input}
+            label="Preço do Etanol"
+            value={eta}
+            onChangeText={text => setEta(text)}
+          />
+          <Button mode="contained" onPress={() => console.log('Pressed')}>
+            Calcular
+          </Button>
+        </View>
+  </View>
 
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  body: {
+    margin: 10,
+  },
+  input: {
+    marginBottom: 10
+  }
+});
 
 export default App;
