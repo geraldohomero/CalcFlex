6132806d3b61b66ce16de24f7148ad27d8c30c00
Geraldo Homero <geraldohomero@protonmail.com>
2023-08-30 20:42:08 -0300

    update - add navigation fuel manager

---
+++ src/pages/Gastos.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './../components/Header';
+import Container from '../components/Container';
+import Body from '../components/Body';
+
+const Gastos = () => {
+    return (
+        <Container>
+            <Header title={'Fuel Manager'}/>
+            <Body>
+            </Body>
+        </Container>
+    );
+};
+
+    export default Gastos;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,17 @@ import React, {useStare} from 'react';
 import { BottomNavigation, Text } from 'react-native-paper';
 
 import Calculadora from './Calculadora';
+import Gastos from './Gastos';
 
 const Home = () => {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
+    { key: 'gastos', title: 'Gastos', icon: 'gas-station' },
     { key: 'calculadora', title: 'Calculadora', icon: 'calculator' },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
+    gastos: Gastos,
     calculadora: Calculadora,
 
   });
