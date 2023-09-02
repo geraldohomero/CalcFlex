e450d2660649efdfa16735da1f84ac1de9bbf119
Geraldo Homero <geraldohomero@protonmail.com>
2023-09-02 17:38:46 -0300

    update

--- src/pages/Gastos.js
+++ src/pages/Gastos.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FlatList, Text, View, StyleSheet } from 'react-native';
-import { List } from 'react-native-paper';
+import { FlatList, View, StyleSheet } from 'react-native';
+import { List, Text, FAB } from 'react-native-paper';
 
 import Header from './../components/Header';
 import Container from '../components/Container';
@@ -8,32 +8,39 @@ import Body from '../components/Body';
 
 const DATA = [
     {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
+      id: 1,
+      tipo: 0,
+      preco: 4.77,
+      data: '01/02/2023',
+      valor: 100,
+      odometro: 22234,
     },
     {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
+      id: 2,
+      tipo: 1,
+      preco: 4.57,
+      data: '23/02/2023',
+      valor: 167,
+      odometro: 22876,
     },
     {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
+      id: 3,
+      tipo: 0,
+      preco: 4.65,
+      data: '26/04/2023',
+      valor: 200,
+      odometro: 23786,
     },
   ];
-
-  const Item = ({ title }) => (
-    <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-    </View>
-    );
 
 const Gastos = () => {
 
     const renderItem = ({ item }) => (
           <List.Item style={styles.ListItem}
-              title="First Item"
-              description="Item description"
-              left={props => <List.Icon {...props} icon="folder" />}
+              title={"R$ " + item.valor.toFixed(2) + " (R$" + item.preco.toFixed(2)+")"}
+              description={item.odometro + " km"}
+              left={props => <List.Icon {...props} color={ item.tipo == 0 ? 'red' : 'green'} icon="gas-station" />}
+              right={props => <Text {...props} style={{alignSelf:'center'}} > {item.data}</Text>}
             />
       );
 
@@ -46,6 +53,11 @@ const Gastos = () => {
               renderItem={renderItem}
               keyExtractor={item => item.id}
             />
+            <FAB
+                style={styles.fab}
+                icon="plus"
+                onPress={() => console.log('Pressed')}
+            />
             </Body>
         </Container>
     );
@@ -53,18 +65,18 @@ const Gastos = () => {
 
 const styles = StyleSheet.create({
     ListItem: {
-      color: '#fff',
+        margin: 5,
+        padding: 5,
+        borderRadius: 5,
+        backgroundColor: '#fff',
+        elevation: 2,
     },
-    item: {
-      color: 'white',
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
-    },
-    title: {
-      fontSize: 32,
-      color: 'white',
-    },
+    fab: {
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+    }
   });
 
 export default Gastos;
