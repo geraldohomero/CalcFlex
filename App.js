364de6871ed671f459f2ac67f8924cea0867877a
Geraldo Homero <geraldohomero@protonmail.com>
2023-10-01 16:06:13 -0300

    update - add navigations

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Home from './src/pages/Home';
 
@@ -6,8 +7,11 @@ import Home from './src/pages/Home';
 const App = () => {
 
   return (
+    <NavigationContainer>
     <Home/>
+    </NavigationContainer>
   )
 }
+
 
 export default App;
---
+++ src/pages/Abastecimento.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './../components/Header';
+import Container from '../components/Container';
+import Body from '../components/Body';
+
+const Abastecimento = () => {
+    return (
+        <Container>
+            <Header title={'Abastecimento'}/>
+            <Body>
+
+            </Body>
+        </Container>
+    );
+};
+
+export default Abastecimento;
